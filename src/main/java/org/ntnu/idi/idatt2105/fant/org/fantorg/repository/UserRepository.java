c4d059186dc919c0ea26da07453fbb17493f5f9e
harui <xulr0820@hotmail.com>
2025-03-22 14:38:15 +0100

    chore(user): Create user repo

--- src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/repository/UserRepository.java
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/repository/UserRepository.java
@@ -1,4 +1,28 @@
 package org.ntnu.idi.idatt2105.fant.org.fantorg.repository;
 
-public class UserRepository {
+import java.util.Optional;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for communication of data layer for user.
+ *
+ * @author Harry Xu
+ * @version 1.0
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Finds a user from a given username
+     * @param username Username
+     * @return Optional user
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * Finds a user from a given email
+     * @param email Email
+     * @return Optional email
+     */
+    Optional<User> findByEmail(String email);
 }
