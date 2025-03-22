57031d0e9f3f210aa809b730bf1b4c494769d3c7
harui <xulr0820@hotmail.com>
2025-03-22 12:02:40 +0100

    Initalized spring repository

---
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/Application.java
@@ -0,0 +1,13 @@
+package org.ntnu.idi.idatt2105.fant.org.fantorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
