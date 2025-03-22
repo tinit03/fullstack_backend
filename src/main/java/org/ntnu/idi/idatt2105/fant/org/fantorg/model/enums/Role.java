10686c00f809ea3bcdaa2c6cf5683dc02cebb19d
harui <xulr0820@hotmail.com>
2025-03-22 14:12:09 +0100

    chore(user): Add role for user

---
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/model/enums/Role.java
@@ -0,0 +1,20 @@
+package org.ntnu.idi.idatt2105.fant.org.fantorg.model.enums;
+
+/**
+ * Granted role for authenticated user
+ *
+ * @author Harry Xu
+ * @version 1.0
+ */
+public enum Role {
+    /**
+     * Regular user
+     */
+    USER,
+
+    /**
+     * Admin
+     */
+    ADMIN,
+}
+
