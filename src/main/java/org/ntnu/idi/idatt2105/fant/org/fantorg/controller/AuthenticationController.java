01f8df44407efeb0566124f695259c9d07c4859b
Tini Tran <tinit@stud.ntnu.no>
2025-03-26 19:55:04 +0100

    Added userRegistration

--- src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/controller/AuthenticationController.java
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/controller/AuthenticationController.java
@@ -4,8 +4,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.JwtTokenDto;
 import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.user.UserLoginDto;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.user.UserRegisterDto;
 import org.ntnu.idi.idatt2105.fant.org.fantorg.model.User;
 import org.ntnu.idi.idatt2105.fant.org.fantorg.security.JwtService;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,15 +24,19 @@ public class AuthenticationController {
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
 
+  private final AuthenticationService userService;
+
   @PostMapping("/login")
   public ResponseEntity<JwtTokenDto> login(@Valid @RequestBody UserLoginDto loginRequest) {
     Authentication authentication = authenticationManager.authenticate(
         (new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword())));
-
     User user = (User) authentication.getPrincipal();
-
     String token = jwtService.generateToken(user,30);
     return ResponseEntity.ok(new JwtTokenDto(token));
-
+  }
+  @PostMapping("/register")
+  public ResponseEntity<Void> register(@Valid @RequestBody UserRegisterDto registerDto) {
+    userService.registerUser(registerDto);
+    return ResponseEntity.ok().build();
   }
 }
--- src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/service/AuthenticationService.java
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/service/AuthenticationService.java
@@ -1,4 +1,31 @@
 package org.ntnu.idi.idatt2105.fant.org.fantorg.service;
 
+import lombok.RequiredArgsConstructor;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.user.UserRegisterDto;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.model.User;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.model.enums.Role;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  public void registerUser(UserRegisterDto dto) {
+    if(userRepository.existsByEmail(dto.getEmail())){
+      throw new IllegalArgumentException("Email already in use!");
+    }
+
+    User user = new User();
+    user.setRole(Role.USER);
+    user.setEmail(dto.getEmail());
+    user.setPassword(passwordEncoder.encode(dto.getPassword()));
+    user.setFirstName(dto.getFirstName());
+    user.setLastName(dto.getLastName());
+    user.setAddress(dto.getAddress());
+
+    userRepository.save(user);
+  }
 }
