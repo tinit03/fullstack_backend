87df631bbece5d5cbd3b3f1ce052bcf5c5db35b1
Tini Tran <tinit@stud.ntnu.no>
2025-03-26 12:41:05 +0100

    Expanded categories so that a category model can self reference for creating a subcategory

--- src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/model/Category.java
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/model/Category.java
@@ -1,10 +1,16 @@
 package org.ntnu.idi.idatt2105.fant.org.fantorg.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,4 +30,16 @@ public class Category {
   private Long categoryId;
 
   private String categoryName;
+
+  @ManyToOne
+  @JoinColumn(name="parent_id")
+  private Category parentCategory; // self referencing for creating the category-subcategory system
+
+  @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL)
+  private List<Category> subCategories = new ArrayList<>();
+
+  // to check if it's a subcategory
+  public boolean isLeaf() {
+    return subCategories == null || subCategories.isEmpty();
+  }
 }
