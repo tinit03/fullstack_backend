b1669eb9a0238ed5b9f1a1db1befcedb17d11d9d
Tini Tran <tinit@stud.ntnu.no>
2025-03-27 15:13:45 +0100

    Added entity, dto, mapper, service, controller for Image

---
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/dto/image/ImageCreateDto.java
@@ -0,0 +1,11 @@
+package org.ntnu.idi.idatt2105.fant.org.fantorg.dto.image;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImageCreateDto {
+  private String url;
+  private String caption;
+}
---
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/mapper/ImageMapper.java
@@ -0,0 +1,37 @@
+package org.ntnu.idi.idatt2105.fant.org.fantorg.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.image.ImageCreateDto;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.image.ImageDto;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.model.Image;
+
+public class ImageMapper {
+
+  public static ImageDto toDto(Image image) {
+    if (image == null) return null;
+
+    ImageDto dto = new ImageDto();
+    dto.setId(image.getId());
+    dto.setUrl(image.getUrl());
+    dto.setCaption(image.getCaption());
+    return dto;
+  }
+
+  public static List<ImageDto> toDtoList(List<Image> images) {
+    return images == null ? null : images.stream().map(ImageMapper::toDto).collect(Collectors.toList());
+  }
+
+  public static Image fromCreateDto(ImageCreateDto dto) {
+    if (dto == null) return null;
+
+    Image image = new Image();
+    image.setUrl(dto.getUrl());
+    image.setCaption(dto.getCaption());
+    return image;
+  }
+
+  public static List<Image> fromCreateDtoList(List<ImageCreateDto> dtos) {
+    return dtos == null ? null : dtos.stream().map(ImageMapper::fromCreateDto).collect(Collectors.toList());
+  }
+}
