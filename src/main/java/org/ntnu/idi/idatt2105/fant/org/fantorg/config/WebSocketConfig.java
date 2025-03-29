331e87a7fcd6c850662ef286f10dd3c3e8b48845
Harry Linrui XU <xulr0820@hotmail.com>
2025-03-29 15:43:37 +0100

    feat(chat): Changing stomp endpoint config + remove event listener for disconnect event

--- src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/config/WebSocketConfig.java
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/config/WebSocketConfig.java
@@ -1,6 +1,13 @@
 package org.ntnu.idi.idatt2105.fant.org.fantorg.config;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -18,6 +25,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.setApplicationDestinationPrefixes("/app");
-    registry.enableSimpleBroker("/topic");
+    registry.enableSimpleBroker("/user");
+    registry.setUserDestinationPrefix("/user");
+  }
+
+  @Override
+  public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+    DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+    resolver.setDefaultMimeType(APPLICATION_JSON);
+    MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+    converter.setObjectMapper(new ObjectMapper());
+    converter.setContentTypeResolver(resolver);
+    messageConverters.add(converter);
+    return false;
   }
 }
