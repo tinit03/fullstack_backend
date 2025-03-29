0316070a0b5e80a3de60931a8ae2b0fb592f67f9
Harry Linrui XU <xulr0820@hotmail.com>
2025-03-29 15:52:12 +0100

    feat(chat): Modify controller, service and repository classes to allow one on one chats + add mapper

--- src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/controller/ChatController.java
+++ src/main/java/org/ntnu/idi/idatt2105/fant/org/fantorg/controller/ChatController.java
@@ -1,30 +1,68 @@
 package org.ntnu.idi.idatt2105.fant.org.fantorg.controller;
 
-import org.ntnu.idi.idatt2105.fant.org.fantorg.model.ChatMessage;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.chat.ChatMessageCreateDto;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.chat.ChatMessageDto;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.dto.chat.ChatNotification;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.model.ChatRoom;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.repository.ChatRoomRepository;
+import org.ntnu.idi.idatt2105.fant.org.fantorg.service.impl.ChatMessageServiceImpl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Slf4j
+@RequiredArgsConstructor
 public class ChatController {
 
-  @MessageMapping("/chat.addUser")
-  @SendTo("/topic/public")
-  public ChatMessage addUser(
-      @Payload ChatMessage chatMessage,
-      SimpMessageHeaderAccessor headerAccessor) {
+  private final ChatMessageServiceImpl chatMessageService;
+  private final SimpMessagingTemplate messagingTemplate;
+  private final ChatRoomRepository chatRoomRepository;
 
-    // Add mail in web socket session
-    headerAccessor.getSessionAttributes().put("mail", chatMessage.getSender());
-    return chatMessage;
+  @GetMapping("/messages/{itemId}/{senderId}/{recipientId}")
+  public ResponseEntity<List<ChatMessageDto>> findChatMessages(
+      @PathVariable("itemId") Long itemId,
+      @PathVariable("senderId") String senderId,
+      @PathVariable("recipientId") String recipientId
+  ) {
+    log.info("Received GET request for /messages/{}/{}/{}", itemId, senderId, recipientId);
+    List<ChatMessageDto> messages = chatMessageService.findChatMessages(senderId, recipientId, itemId);
+    log.info("messages: {}", messages);
+    return ResponseEntity.ok(messages);
   }
 
-  @MessageMapping("/chat.sendMessage")
-  @SendTo("/topic/public")
-  public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-    return chatMessage;
+  @MessageMapping("/chat")
+  public void processMessage(@Payload ChatMessageCreateDto messageDto) {
+    log.info("Received message for /chat");
+    log.info("Received chat msg: '{}'", messageDto.getContent());
+    log.info("Saving chat msg: '{}'...", messageDto.getContent());
+    ChatMessageDto savedMsgDto = chatMessageService.save(messageDto);
+
+    log.info("Saved chat msg: '{}'", savedMsgDto);
+
+    log.info("Sending message to /{}/queue/messages", savedMsgDto.getRecipientId());
+    messagingTemplate.convertAndSendToUser(savedMsgDto.getRecipientId(), "/queue/messages/" + messageDto.getItemId(),
+        ChatNotification.builder()
+            .id(savedMsgDto.getId())
+            .senderId(savedMsgDto.getSenderId())
+            .recipientId(savedMsgDto.getRecipientId())
+            .itemId(savedMsgDto.getItemId())
+            .content(savedMsgDto.getContent())
+            .build()
+    );
+    log.info("Sent to recipient");
   }
 
+  @GetMapping("/getChats")
+  public List<ChatRoom> getChatRooms() {
+    return chatRoomRepository.findAll();
+  }
 }
