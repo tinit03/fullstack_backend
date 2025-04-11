622d458c13079eb46546481d24d7c057ba76140c
HarXu <Harry.Xu@triona.no>
2025-04-11 07:18:08 +0200

    test: remove redudant stub

--- src/test/java/org/ntnu/idi/idatt2105/fant/org/fantorg/unit/service/OrderServiceImplTest.java
+++ src/test/java/org/ntnu/idi/idatt2105/fant/org/fantorg/unit/service/OrderServiceImplTest.java
@@ -94,8 +94,6 @@ public class OrderServiceImplTest {
             .content(null)
             .build();
 
-    when(chatMessageService.save(any(chatMessageCreateDto.getClass()))).thenReturn(null);
-
     OrderDto result = orderService.createOrder(orderCreateDto, buyer);
 
     assertThat(result).isNotNull();
